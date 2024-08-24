754abfd4368f43e8deab692bdb7b9f096fd4a8a8
Dexatr <jaguarmalls74@protonmail.com>
2024-08-24 23:08:47 +0000

    Complete Assignment 5 Part 2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,29 +5,24 @@
 ##############################################################
 
 # Updated with the correct git commit hash for assignment 3
-AESD_ASSIGNMENTS_VERSION = '51ae4619780f164ede292dac1ff6eb07e2ac88a2'
+AESD_ASSIGNMENTS_VERSION = '1cfb8ff7fc4b1cceeb75e13b6e995088457ec3b3'
 # Ensure the SSH repository URL is correct
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-Dexatr.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
-# Installation steps for writer, finder, and finder-test utilities/scripts
+# Installation steps for aesdsocket utility and startup script
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	# Create necessary directories
-	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf/
-	# Install configuration files
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	# Install test scripts into /usr/bin
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/usr/bin/
-	# Install the writer utility
-	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
-	# Install the finder and finder-test scripts into /usr/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
+	# Install the aesdsocket executable to /usr/bin
+	$(INSTALL) -D -m 755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	# Install the startup script to /etc/init.d/S99aesdsocket
+	$(INSTALL) -D -m 755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	# Install any other necessary files (if required)
+	# $(INSTALL) -m 755 $(@D)/other-files $(TARGET_DIR)/path/to/destination/
 endef
 
 $(eval $(generic-package))
