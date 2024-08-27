ce2ee21542bbc1f037cf1425430328aef5fe3471
Dexatr <jaguarmalls74@protonmail.com>
2024-08-27 18:33:33 +0000

    Complete Assignment 8

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 # Updated with the correct git commit hash for assignment 3
-AESD_ASSIGNMENTS_VERSION = '1cfb8ff7fc4b1cceeb75e13b6e995088457ec3b3'
+AESD_ASSIGNMENTS_VERSION = '53b7fbb743fc9c0496d422fbe684d0ac09787f3c'
 # Ensure the SSH repository URL is correct
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-Dexatr.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
@@ -20,7 +20,7 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	# Install the aesdsocket executable to /usr/bin
 	$(INSTALL) -D -m 755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
 	# Install the startup script to /etc/init.d/S99aesdsocket
-	$(INSTALL) -D -m 755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -D -m 755 $(@D)/server/S99aesdsocket $(TARGET_DIR)/etc/init.d/S99aesdsocket
 	# Install any other necessary files (if required)
 	# $(INSTALL) -m 755 $(@D)/other-files $(TARGET_DIR)/path/to/destination/
 endef
